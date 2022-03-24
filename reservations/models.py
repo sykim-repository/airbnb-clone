741bbd81b72c2ea660ed23106fc2e1c42daa16b7
sykim <suyeongkim@MacBook-Pro.local>
2022-03-25 00:57:38 +0900

    0325 push

--- reservations/models.py
+++ reservations/models.py
@@ -20,8 +20,12 @@ class Reservation(core_models.TimeStampedModel):
     status = models.CharField(
         max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
     )
-    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    guest = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=models.CASCADE
+    )
     check_in = models.DateField()
     check_out = models.DateField()
 
--- rooms/admin.py
+++ rooms/admin.py
@@ -8,6 +8,11 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Definition"""
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -20,4 +25,79 @@ class PhotoAdmin(admin.ModelAdmin):
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    """Room Admin Definition"""
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "bedrooms", "beds", "baths")},
+        ),
+        (
+            "More About the Space",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "beds",
+        "bedrooms",
+        "baths",
+        "guests",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    search_fields = ["=city", "^host__username"]
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    ordering = (
+        "name",
+        "price",
+        "bedrooms",
+    )
+
+    def count_amenities(self, obj):
+        print(obj.amenities.count())
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        print(obj.photos.count())
+        return obj.photos.count()
+
+    count_amenities.short_description = "cnt_amenities"
